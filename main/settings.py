f9be1c4e87a07c7be1c9ffb94c9a332dc566c777
SaviourBassey <saviourbazi2021@yahoo.com>
2025-12-24 19:29:27 +0100

    first commit

--- main/settings.py
+++ main/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -84,7 +85,7 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASE_URL = config('DATABASE_URL')
 
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
@@ -140,6 +141,8 @@ STATIC_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
